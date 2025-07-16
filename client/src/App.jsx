c8736dcecf2d7d4247acde814caca475db087071
Mritunjay Pandey <pmritunjay9555@gmail.com>
2025-07-16 21:18:51 +0530

    Project Completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,14 @@ import BuyCredit from './pages/BuyCredit'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
 
 const App = () => {
   return (
     <div className='min-h-screen bg-slate-50'>
+      <ToastContainer position='bottom-right' />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
 import userRouter from './routes/userRoutes.js'
+import imageRouter from './routes/imageRoutes.js'
 
 
 //App Config
@@ -17,5 +18,6 @@ app.use(cors())
 //API route
 app.get('/',(req,res)=>res.send("API Working"))
 app.use('/api/user',userRouter)
+app.use('/api/image', imageRouter)
 
 app.listen(PORT,()=>console.log("Server Running on port"+PORT))
